7668a1c3526233d27620ec7c7fa0f016ba37c0b1
Abakan <abakano21@gmail.com>
2021-06-16 21:18:30 +0400

    wip

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-ini_set('max_execution_time', 120); 
-set_time_limit(50);
+
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
